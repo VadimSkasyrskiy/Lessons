2aa2c201058fce75d29a2a1cf3b7f0fba7bb98b3
VadimSkasyrskiy <v.skasurskii@gmail.com>
2020-09-10 21:05:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine(" ==== 1");
+                    break;
+                case 2:
+                    Console.WriteLine("======== 2");
+                    break;
+
+                default:
+                    Console.WriteLine("no var");
+                    break;
+            }
+        }
+    }
+}
